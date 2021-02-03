5438597bb694ae54babdafc795bbd781ac84680e
Alastair Cooper <alastair.cooper@outlook.com>
2021-02-03 17:08:45 +0000

    added example window code

--- guppy.py
+++ guppy.py
@@ -1,1 +1,15 @@
-print("\nHello from Guppy!\n")
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+def on_activate(app):
+    win = Gtk.ApplicationWindow(application=app)
+    btn = Gtk.Button(label="Hello from Guppy!")
+    btn.connect('clicked', lambda x: win.close())
+    win.add(btn)
+    win.show_all()
+
+app = Gtk.Application(application_id='org.gtk.Example')
+app.connect('activate', on_activate)
+app.run(None)
